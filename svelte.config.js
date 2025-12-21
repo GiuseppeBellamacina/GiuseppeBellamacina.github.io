d97ea33a5ce4908af4c3de4bb9b54293d31fa8a0
Giuseppe Bellamacina <bellamacina50@gmail.com>
2025-12-21 12:17:01 +0100

    ci(deploy): add .nojekyll file for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -15,9 +15,12 @@ const config = {
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
-			precompress: true,
+			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '' : ''
+		}
 	}
 };
 
